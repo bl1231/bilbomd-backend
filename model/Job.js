8b0b0a0b2df250f892215c38db630654ce44e59a
Scott Classen <sclassen@lbl.gov>
2023-01-27 16:26:39 -0800

    establish many-to-one relationship between Job and User

--- model/Job.js
+++ model/Job.js
@@ -2,36 +2,31 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const jobSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    segments: { type: Number, required: true },
-    segment_1: { type: String },
-    segment_2: { type: String },
-    segment_3: { type: String },
-    segment_4: { type: String },
-    segment_5: { type: String },
-    segment_6: { type: String },
-    segment_7: { type: String },
-    segment_8: { type: String },
-    segment_9: { type: String },
-    segment_10: { type: String },
-    const_inp_file: { type: String },
-    data_file: { type: String, required: true },
-    conformational_sampling: {
-        type: Number,
-        enum: [200, 400, 600, 800],
-        default: 200
-    },
-    rg_min: { type: Number, reqired: true, minimum: 10, maximum: 100 },
-    rg_max: { type: Number, reqired: true, minimum: 10, maximum: 100 },
-    status: {
-        type: String,
-        enum: ['Submitted', 'Pending', 'Running', 'Completed', 'Error'],
-        default: 'Submitted'
-    },
-    time_submitted: Date
+  title: {
+    type: String,
+    required: true
+  },
+  const_inp_file: { type: String },
+  data_file: { type: String, required: true },
+  conformational_sampling: {
+    type: Number,
+    enum: [200, 400, 600, 800],
+    default: 200
+  },
+  rg_min: { type: Number, reqired: true, minimum: 10, maximum: 100 },
+  rg_max: { type: Number, reqired: true, minimum: 10, maximum: 100 },
+  status: {
+    type: String,
+    enum: ['Submitted', 'Pending', 'Running', 'Completed', 'Error'],
+    default: 'Submitted'
+  },
+  time_submitted: Date,
+  time_started: Date,
+  time_completed: Date,
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
 });
 
 module.exports = mongoose.model('Job', jobSchema);
--- model/User.js
+++ model/User.js
@@ -2,37 +2,44 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true
+  username: {
+    type: String,
+    required: true
+  },
+  roles: {
+    User: {
+      type: Number,
+      default: 2001
     },
-    roles: {
-        User: {
-            type: Number,
-            default: 2001
-        },
-        Editor: Number,
-        Admin: Number
-    },
-    refreshToken: [String],
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    status: {
-        type: String,
-        enum: ['Pending', 'Active'],
-        default: 'Pending'
-    },
-    confirmationCode: {
-        type: String
-    },
-    otp: { type: String },
-    UUID: {
-        type: String
-    },
-    createdAt: Date
+    Editor: Number,
+    Admin: Number
+  },
+  refreshToken: [String],
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  status: {
+    type: String,
+    enum: ['Pending', 'Active'],
+    default: 'Pending'
+  },
+  confirmationCode: {
+    type: String
+  },
+  otp: { type: String },
+  UUID: {
+    type: String
+  },
+  createdAt: Date,
+  last_access: Date,
+  jobs: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Job'
+    }
+  ]
 });
 
 module.exports = mongoose.model('User', userSchema);
