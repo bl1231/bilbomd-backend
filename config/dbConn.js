5d5a6d290f40a9b872eb3512578d2f925bba9942
Scott Classen <sclassen@lbl.gov>
2023-02-14 12:30:37 -0800

    add dockerfile and test init docker compose up

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,15 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+
+const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOSTNAME, MONGO_PORT, MONGO_DB } =
+  process.env
+
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  connectTimeoutMS: 10000
+}
 
 const connectDB = async () => {
   try {
-    await mongoose.set('strictQuery', false);
-    await mongoose.connect(process.env.DATABASE_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
-    });
+    await mongoose.set('strictQuery', false)
+    await mongoose.connect(url, options)
   } catch (err) {
-    console.error(err);
+    console.error('here--> ', err)
   }
-};
+}
 
-module.exports = connectDB;
+module.exports = connectDB
