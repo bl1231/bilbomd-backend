de64e79f3415f0deb241972e615a00355f6e07e9
Scott Classen <sclassen@lbl.gov>
2023-03-27 07:59:05 -0700

    sorting out CORS errors. add all possible hosts

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,19 @@
 const allowedOrigins = [
-  'http://localhost:3500',
   'http://localhost:3001',
-  'http://localhost:3000',
-  'https://bl1231-local.als.lbl.gov'
-];
+  'http://localhost:3500',
+  'http://127.0.0.1:3001',
+  'http://127.0.0.1:3500',
+  'http://192.168.1.1:3001',
+  'http://192.168.1.1:3500',
+  'http://192.168.40.7:3001',
+  'http://192.168.40.7:3500',
+  'http://192.168.1.7:3001',
+  'http://192.168.1.7:3500',
+  'http://192.168.1.104:80',
+  'http://192.168.1.104:80',
+  'https://192.168.1.104:443',
+  'https://192.168.1.104:443',
+  'https://bl1231.als.lbl.gov'
+]
 
-module.exports = allowedOrigins;
+module.exports = allowedOrigins
