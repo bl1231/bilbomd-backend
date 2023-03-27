7a86db1322b4e5d8c090ad43f62f89a8bf4b3442
Scott Classen <sclassen@lbl.gov>
2023-03-27 14:06:39 -0700

    add defaultJobOptions section to Queue constructor

--- queues/jobQueue.js
+++ queues/jobQueue.js
@@ -4,12 +4,15 @@ const bilbomdQueue = new Queue('bilbomd', {
   connection: {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT
+  },
+  defaultJobOptions: {
+    attempts: 3,
+    priority: 1
   }
 })
 
 const queueJob = async (job) => {
   await bilbomdQueue.add(job.title, job)
-  return
 }
 
 module.exports = { queueJob }
