21e9d1326ff44f73b08f1137ae4fec73e484f08f
Scott Classen <sclassen@lbl.gov>
2023-05-25 16:08:43 -0700

    add ability to resend email verification code

--- controllers/verifyController.js
+++ controllers/verifyController.js
@@ -1,4 +1,7 @@
 const User = require('../model/User')
+const { sendVerificationEmail } = require('../config/nodemailerConfig')
+const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const { BILBOMD_URL } = process.env
 
 const verifyNewUser = async (req, res) => {
   if (!req?.body?.code) {
@@ -23,4 +26,47 @@ const verifyNewUser = async (req, res) => {
   }
   res.json({ message: 'verified' })
 }
-module.exports = { verifyNewUser }
+const resendVerificationCode = async (req, res) => {
+  const { email } = req.body
+  console.log('request to resendVerificationCode for:', email)
+
+  // confirm we have required data
+  if (!email)
+    return res.status(400).json({
+      message: 'email required.'
+    })
+
+  // check for user in the db
+  const foundUser = await User.findOne({ email: email }).exec()
+
+  if (!foundUser) return res.status(401).json({ message: 'no user with that email' })
+  // console.log('resendVerificationCode found user:', foundUser)
+  try {
+    //create a new confirmation code
+    let confirmationCode = ''
+    for (let i = 0; i < 36; i++) {
+      confirmationCode += characters[Math.floor(Math.random() * characters.length)]
+    }
+
+    // add verification code to the Users MongoDB entry
+    foundUser.confirmationCode = confirmationCode
+    const result = await foundUser.save()
+    console.log(
+      'updated user: ',
+      result.username,
+      'email: ',
+      result.email,
+      'code: ',
+      result.confirmationCode
+    )
+
+    //send Verification email
+    sendVerificationEmail(email, BILBOMD_URL, confirmationCode)
+
+    res.status(201).json({ message: 'ok' })
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({ message: 'invalid user data received' })
+  }
+}
+module.exports = { verifyNewUser, resendVerificationCode }
--- routes/verify.js
+++ routes/verify.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const router = express.Router();
-const verifyController = require("../controllers/verifyController");
+const express = require('express')
+const router = express.Router()
+const verifyController = require('../controllers/verifyController')
 
-router.post("/", verifyController.verifyNewUser);
+router.route('/').post(verifyController.verifyNewUser)
+router.route('/resend').post(verifyController.resendVerificationCode)
 
-module.exports = router;
+module.exports = router
