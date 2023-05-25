ebe642ec306afcda38d45631c615dca2ac8d7c94
Scott Classen <sclassen@lbl.gov>
2023-05-25 13:20:45 -0700

    comment console logs - do not send entire mongodb result back to client.

--- controllers/verifyController.js
+++ controllers/verifyController.js
@@ -6,17 +6,21 @@ const verifyNewUser = async (req, res) => {
   }
   console.log('verification code:', req.body.code)
   const user = await User.findOne({ confirmationCode: req.body.code }).exec()
+  console.log('verification code belongs to user:', user?.username, user?.email)
+
   if (!user) {
     return res.status(204).json({ message: `Unable to verify ${req.body.code}.` })
   }
-  console.log('unverified user:', user)
 
+  // console.log('unverified user:', user)
+
+  // Set status to "Active" and delete the conformationCode
   if (req.body?.code) {
     user.status = 'Active'
     user.confirmationCode = undefined
     const result = await user.save()
-    console.log('save verified user to MongoDB:', result)
-    res.json(result)
+    //console.log('save verified user to MongoDB:', result)
   }
+  res.json({ message: 'verified' })
 }
 module.exports = { verifyNewUser }
