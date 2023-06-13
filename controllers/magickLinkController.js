853ad11777d325e6c2ebf06a4d9e2e073416871a
Scott Classen <sclassen@lbl.gov>
2023-06-13 15:49:45 -0700

    improve magicklink controller - use crypto instead of hand-rolled code generator - destructure req.body in params

--- controllers/magickLinkController.js
+++ controllers/magickLinkController.js
@@ -1,42 +1,40 @@
 const { logger } = require('../middleware/loggers')
-
+const crypto = require('crypto')
 const User = require('../model/User')
-const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 const { sendMagickLinkEmail } = require('../config/nodemailerConfig')
-
 const { BILBOMD_URL } = process.env
 
-const generateMagickLink = async (req, res) => {
-  const { email } = req.body
+const generateMagickLink = async ({ body: { email } }, res) => {
   if (!email) {
     return res.status(400).json({ message: 'email is required' })
   }
 
-  const foundUser = await User.findOne({ email: email }).exec()
-  if (!foundUser) return res.status(401).json({ message: 'no account with that email' }) // unauthorized.
-  // Refuse to generate OTP if user is "Pending"
-  if (foundUser.status == 'Pending')
-    return res.status(403).json({ message: 'Pending', email: email })
-  if (foundUser.active == false)
-    return res.status(403).json({ message: 'account deactivated' })
-  try {
-    // generate a 34 character One Time Password (OTP)
-    let passcode = ''
-    for (let i = 0; i < 34; i++) {
-      passcode += characters[Math.floor(Math.random() * characters.length)]
-    }
+  const foundUser = await User.findOne({ email }).exec()
 
-    // 120000ms = 2minutes
-    // 3600000ms = 1hour
+  if (!foundUser) {
+    return res.status(401).json({ message: 'no account with that email' })
+  }
+
+  if (foundUser.status === 'Pending') {
+    return res.status(403).json({ message: 'Pending', email })
+  }
+
+  if (!foundUser.active) {
+    return res.status(403).json({ message: 'account deactivated' })
+  }
+
+  try {
+    const passcode = crypto.randomBytes(17).toString('hex')
+
     const otp = { code: passcode, expiresAt: new Date(Date.now() + 3600000) }
-    // add OTP to the Users MongoDB entry
     foundUser.otp = otp
     await foundUser.save()
+
     const message = 'magicklink requested by %s send OTP: %s'
     logger.info(message, foundUser.email, passcode)
 
-    //send MagickLink email
+    // Send MagickLink email
     sendMagickLinkEmail(email, BILBOMD_URL, passcode)
 
     res.status(201).json({ success: `OTP created for ${email}`, otp: passcode })
