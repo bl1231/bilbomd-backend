1e3e251c1b6371e26e5d999c0a3343c91251849a
Scott Classen <sclassen@lbl.gov>
2023-06-13 09:46:47 -0700

    clean up - and add note about docker vs.non-docker deployment - improve the console log messages

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require('./app')
-// const { bilbomdQueue } = require('./queues/jobQueue')
-const PORT = process.env.BILBOMD_BACKEND_PORT || 3500
-// const PORT = 3500
+
+// PORT should always be 3500 when running in Docker container!!!!
+//const PORT = process.env.BILBOMD_BACKEND_PORT || 3500
+const PORT = 3500
 
 const server = app.listen(PORT, () =>
   console.log(`BilboMD server starting on port ${PORT}`)
@@ -10,9 +11,9 @@ const server = app.listen(PORT, () =>
 // Cleanup logic
 const cleanup = async () => {
   if (server !== null) {
-    console.log('Closing ExpressJS server')
+    console.log('Closing BilboMD ExpressJS server')
     await server.close((err) => {
-      console.log('Closed ExpressJS server')
+      console.log('Closed BilboMD ExpressJS server')
       process.exit(err ? 1 : 0)
     })
   }
@@ -20,13 +21,13 @@ const cleanup = async () => {
 
 // Handle process termination signals
 process.on('SIGINT', async () => {
-  console.log('Received SIGINT signal in server.js')
+  console.log('Received SIGINT ... shutting down BilboMD')
   await cleanup()
   process.exit(0)
 })
 
 process.on('SIGTERM', async () => {
-  console.log('Received SIGTERM signal in server.js')
+  console.log('Received SIGTERM ... shutting down BilboMD')
   await cleanup()
   process.exit(0)
 })
