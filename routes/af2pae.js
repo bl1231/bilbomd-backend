efe01de20f4a29dc5e8d51628e462a80f7726d9e
Scott Classen <sclassen@lbl.gov>
2023-09-05 13:34:39 -0700

    add downloadConstFile route

--- routes/af2pae.js
+++ routes/af2pae.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router()
 // const af2paeController = require('../controllers/af2paeController')
-const { createNewConstFile } = require('../controllers/af2paeController')
+const {
+  createNewConstFile,
+  downloadConstFile
+} = require('../controllers/af2paeController')
 const verifyJWT = require('../middleware/verifyJWT')
 
 // router.use(verifyJWT)
 
-router.post('/', createNewConstFile)
+router.route('/').get(downloadConstFile).post(createNewConstFile)
+
+// router.route('/:uuid/const.inp').get(downloadConstFile)
 
 module.exports = router
