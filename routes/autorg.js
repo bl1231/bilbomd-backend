21f2cdb2be2b0a406bb3c02cb449b3a8bb4dc27b
Scott Classen <sclassen@lbl.gov>
2023-10-02 16:32:34 -0700

    add new AutoRg route

---
+++ routes/autorg.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const jobController = require('../controllers/jobsController')
+// const verifyJWT = require('../middleware/verifyJWT')
+
+// router.use(verifyJWT)
+
+router.route('/').post(jobController.getAutoRg)
+
+module.exports = router
