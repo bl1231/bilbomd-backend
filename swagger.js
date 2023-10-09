6792989689da4db7cb06c9c5458caaf23602f8ea
Scott Classen <sclassen@lbl.gov>
2023-10-09 16:43:54 -0700

    add code to convert mongoose schema to swagger

--- swagger.js
+++ swagger.js
@@ -1,4 +1,20 @@
 const fs = require('fs')
 const swaggerSpec = require('./swaggerOptions')
+const m2s = require('mongoose-to-swagger')
 
+const User = require('./model/User')
+const Job = require('./model/Job')
+
+// Generate the Swagger definition for Mongoose/MongoDB schema
+const userSwaggerDefinition = m2s(User)
+const jobSwaggerDefinition = m2s(Job)
+
+// Merge the User schema definition into your existing Swagger spec
+// Add the User schema definition to your Swagger document
+swaggerSpec.components = swaggerSpec.components || {}
+swaggerSpec.components.schemas = swaggerSpec.components.schemas || {}
+swaggerSpec.components.schemas.User = userSwaggerDefinition
+swaggerSpec.components.schemas.Job = jobSwaggerDefinition
+
+// Write the updated Swagger JSON to a file
 fs.writeFileSync('swagger.json', JSON.stringify(swaggerSpec, null, 2))
