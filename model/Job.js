6afc15c07f3e087e5abf84c15eb22ea7218cadcf
Scott Classen <sclassen@lbl.gov>
2023-10-11 08:59:20 -0700

    define new Mongoose models add new route

--- model/Job.js
+++ model/Job.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-// const AutoIncrement = require('mongoose-sequence')(mongoose)
 const Schema = mongoose.Schema
 
 const jobSchema = new Schema(
@@ -11,15 +10,7 @@ const jobSchema = new Schema(
     uuid: { type: String, required: true },
     psf_file: { type: String, required: true },
     crd_file: { type: String, required: true },
-    const_inp_file: { type: String },
     data_file: { type: String, required: true },
-    conformational_sampling: {
-      type: Number,
-      enum: [1, 2, 3, 4],
-      default: 1
-    },
-    rg_min: { type: Number, required: true, minimum: 10, maximum: 100 },
-    rg_max: { type: Number, required: true, minimum: 10, maximum: 100 },
     status: {
       type: String,
       enum: ['Submitted', 'Pending', 'Running', 'Completed', 'Error'],
@@ -39,10 +30,41 @@ const jobSchema = new Schema(
   }
 )
 
-// jobSchema.plugin(AutoIncrement, {
-//   inc_field: 'ticket',
-//   id: 'ticketNums',
-//   start_seq: 1
-// })
+const bilboMdJobSchema = new Schema(
+  {
+    const_inp_file: { type: String, required: true },
+    conformational_sampling: {
+      type: Number,
+      enum: [1, 2, 3, 4],
+      default: 1
+    },
+    rg_min: { type: Number, required: true, min: 10, max: 100 },
+    rg_max: { type: Number, required: true, min: 10, max: 100 }
+  },
+  {
+    timestamps: true
+  }
+)
 
-module.exports = mongoose.model('Job', jobSchema)
+const bilboMdAutoJobSchema = new Schema(
+  {
+    const_inp_file: { type: String, required: false },
+    pae_file: { type: String, required: true },
+    conformational_sampling: {
+      type: Number,
+      enum: [1, 2, 3, 4],
+      default: 1
+    },
+    rg_min: { type: Number },
+    rg_max: { type: Number }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Job = mongoose.model('Job', jobSchema)
+const BilboMdJob = Job.discriminator('BilboMd', bilboMdJobSchema)
+const BilboMdAutoJob = Job.discriminator('BilboMdAuto', bilboMdAutoJobSchema)
+
+module.exports = { Job, BilboMdJob, BilboMdAutoJob }
