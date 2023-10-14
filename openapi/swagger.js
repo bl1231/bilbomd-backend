300b78f55d10f22cb7d097195464bbde097dfc6f
Scott Classen <sclassen@lbl.gov>
2023-10-14 12:33:28 -0700

    swagger update

--- openapi/swagger.js
+++ openapi/swagger.js
@@ -3,18 +3,24 @@ const swaggerSpec = require('./swaggerOptions')
 const m2s = require('mongoose-to-swagger')
 
 const User = require('../model/User')
-const Job = require('../model/Job')
+// const Job = require('../model/JobTest')
+// const BilboMdJob = require('../model/Job')
+// const BilboMdAutoJob = require('../model/Job')
 
 // Generate the Swagger definition for Mongoose/MongoDB schema
 const userSwaggerDefinition = m2s(User)
-const jobSwaggerDefinition = m2s(Job)
+// const jobSwaggerDefinition = m2s(Job)
+// const bilboMdJobSwaggerDefinition = m2s(BilboMdJob)
+// const bilboMdAutoJobSwaggerDefinition = m2s(BilboMdAutoJob)
 
 // Merge the User schema definition into your existing Swagger spec
 // Add the User schema definition to your Swagger document
 swaggerSpec.components = swaggerSpec.components || {}
 swaggerSpec.components.schemas = swaggerSpec.components.schemas || {}
 swaggerSpec.components.schemas.User = userSwaggerDefinition
-swaggerSpec.components.schemas.Job = jobSwaggerDefinition
+// swaggerSpec.components.schemas.Job = jobSwaggerDefinition
+// swaggerSpec.components.schemas.BilboMDJob = bilboMdJobSwaggerDefinition
+// swaggerSpec.components.schemas.BilboMDAutoJob = bilboMdAutoJobSwaggerDefinition
 
 // Add Bearer authentication
 swaggerSpec.security = [
