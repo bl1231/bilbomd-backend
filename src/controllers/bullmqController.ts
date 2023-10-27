357aaa181de6f4a80905c7b2e90f7e96545be0f8
Scott Classen <sclassen@lbl.gov>
2023-10-27 10:25:24 -0700

    first pass at bullmqController.ts

---
+++ src/controllers/bullmqController.ts
@@ -0,0 +1,19 @@
+// import { logger } from '../middleware/loggers'
+import { getActiveCount, getWaitingCount, getWorkers } from '../queues/jobQueue'
+import { Request, Response } from 'express'
+
+const getQueueStatus = async (req: Request, res: Response) => {
+  const bullmqActiveCount = await getActiveCount()
+  const bullmqWaitCount = await getWaitingCount()
+  const bullmqWorkerCount = (await getWorkers()).length
+  const queueStatus = {
+    name: 'bilbomd',
+    active_count: bullmqActiveCount,
+    waiting_count: bullmqWaitCount,
+    worker_count: bullmqWorkerCount
+  }
+  // logger.info(queueStatus)
+  res.json(queueStatus)
+}
+
+export { getQueueStatus }
