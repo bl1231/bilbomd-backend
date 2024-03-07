16574dbd6a21c17fd0400f15ba51a0a9ba190636
Scott Classen <sclassen@lbl.gov>
2024-03-07 13:53:12 -0800

    changes to the dev logic for transpiling and restarting nodemon

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,34 @@
 import app from './app'
+import { logger } from './middleware/loggers'
 
-// PORT should always be 3500 when running in Docker container!!!!
-// const PORT = process.env.BILBOMD_BACKEND_PORT || 3500
 const PORT = 3500
 
 const server = app.listen(PORT, () =>
-  console.log(`BilboMD server starting on port ${PORT}`)
+  logger.info(`BilboMD server starting on port ${PORT}`)
 )
 
 // Cleanup logic
-const cleanup = async () => {
-  if (server !== null) {
-    console.log('Closing BilboMD ExpressJS server')
-    await server.close((err) => {
-      console.log('Closed BilboMD ExpressJS server')
-      process.exit(err ? 1 : 0)
-    })
-  }
+const cleanup = () => {
+  logger.info('Closing BilboMD ExpressJS server')
+  server.close((err) => {
+    logger.info('Closed BilboMD ExpressJS server')
+    if (err) {
+      console.error('Server shutdown error:', err)
+      process.exit(1)
+    } else {
+      logger.info('Server gracefully shut down.')
+      process.exit(0)
+    }
+  })
 }
 
 // Handle process termination signals
-process.on('SIGINT', async () => {
-  console.log('Received SIGINT ... shutting down BilboMD')
-  await cleanup()
-  process.exit(0)
+process.on('SIGINT', () => {
+  logger.info('Received SIGINT ... shutting down BilboMD')
+  cleanup()
 })
 
-process.on('SIGTERM', async () => {
-  console.log('Received SIGTERM ... shutting down BilboMD')
-  await cleanup()
-  process.exit(0)
+process.on('SIGTERM', () => {
+  logger.info('Received SIGTERM ... shutting down BilboMD')
+  cleanup()
 })
