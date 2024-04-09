e42a18ff2fbfebe542a7d4ad0cf636ff92b1f497
Scott Classen <sclassen@lbl.gov>
2024-04-09 15:50:11 -0700

    get some basic plumbing in place for generating swaggerui docs

---
+++ src/openapi/swaggerOptions.ts
@@ -0,0 +1,35 @@
+import swaggerJsdoc from 'swagger-jsdoc'
+// import dotenv from 'dotenv'
+
+// Initialize dotenv to use environment variables if not already done elsewhere in your project
+// dotenv.config()
+
+const VERSION = process.env.npm_package_version
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'BilboMD Backend',
+      version: VERSION || 'w.t.f', // Provide a fallback version if npm_package_version is not set
+      description: 'API documentation for bilbomd-backend'
+    },
+    servers: [
+      {
+        url: 'https://bl1231.als.lbl.gov/api/v1',
+        description: 'production'
+      },
+      {
+        url: 'http://localhost:3501/api/v1',
+        description: 'development'
+      }
+    ]
+  },
+  apis: ['src/controllers/*.ts'] // Make sure this glob pattern matches your actual TypeScript files location
+}
+
+const swaggerSpecJson = swaggerJsdoc(options)
+
+console.log(swaggerSpecJson)
+
+export default swaggerSpecJson
