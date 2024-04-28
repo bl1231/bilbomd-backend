6ce807e7e8ee54ea090f3b8b53b6ba1d085bd38c
Scott Classen <sclassen@lbl.gov>
2024-04-28 08:33:10 -0700

    add code to save params.json to upload folder

--- src/controllers/jobsController.ts
+++ src/controllers/jobsController.ts
@@ -358,6 +358,17 @@ const handleBilboMDJob = async (
     await newJob.save()
     logger.info(`BilboMD-${bilbomdMode} Job saved to MongoDB: ${newJob.id}`)
 
+    // Save Job params to $UUID for use by NERSC job script.
+    const nerscParams = JSON.stringify(jobData, null, 2)
+    try {
+      const jobDir = path.join(uploadFolder, UUID)
+      const paramsFile = path.join(jobDir, 'params.json')
+      fs.writeFileSync(paramsFile, nerscParams)
+      logger.info(`Saved ${paramsFile}`)
+    } catch (error) {
+      logger.error(`Unable to save params.json ${error}`)
+    }
+
     // Queue the job
     const BullId = await queueJob({
       type: bilbomdMode,
