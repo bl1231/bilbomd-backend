2751edaf0561588a4dc8dbba9eeedc52c2d6c82c
Scott Classen <sclassen@lbl.gov>
2024-08-22 11:28:58 -0700

    remove commented code

--- src/routes/configs.ts
+++ src/routes/configs.ts
@@ -1,10 +1,7 @@
 import express from 'express'
 import { getConfigsStuff } from '../controllers/configsController.js'
-// import verifyJWT from '../middleware/verifyJWT.js'
 
 const router = express.Router()
-
-// router.use(verifyJWT)
 
 router.route('/').get(getConfigsStuff)
 
