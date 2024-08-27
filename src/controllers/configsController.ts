b0d37a41663b393b3d8770a8b3e43c8e3e014810
Scott Classen <sclassen@lbl.gov>
2024-08-27 09:37:42 -0700

    refactore configsController

--- src/controllers/configsController.ts
+++ src/controllers/configsController.ts
@@ -1,32 +1,52 @@
-import { Request, Response } from 'express'
-import { logger } from '../middleware/loggers.js'
-import axios from 'axios'
+import { Request, Response } from 'express';
+import { logger } from '../middleware/loggers.js';
+import axios from 'axios';
 
 export const getConfigsStuff = async (req: Request, res: Response) => {
-  logger.info(`--- getConfigsStuff ---`)
-  logger.info(`process.env.SFAPI_TOKEN_EXPIRES: ${process.env.SFAPI_TOKEN_EXPIRES}`)
-  logger.info(`process.env.USE_NERSC: ${process.env.USE_NERSC}`)
-  logger.info(`process.env.NERSC_PROJECT: ${process.env.NERSC_PROJECT}`)
+  logger.info('--- getConfigsStuff ---');
 
-  const workerInfo = await axios.get('http://worker-service/worker-info')
+  try {
+    // Fetch worker info
+    const { data: workerInfo } = await axios.get(
+      `${process.env.WORKER_SERVICE_URL || 'http://worker'}:${
+        process.env.WORKER_SERVICE_PORT || 3000
+      }/worker-info`
+    );
 
-  const configs = {
-    tokenExpires: process.env.SFAPI_TOKEN_EXPIRES || '2024-05-22 04:20',
-    useNersc: process.env.USE_NERSC || 'false',
-    nerscProject: process.env.NERSC_PROJECT || 'm1234',
-    sendMailUser: process.env.SENDMAIL_USER || 'bilbomd@lbl.gov',
-    gitHash: process.env.GIT_HASH || '',
-    mode: process.env.BILBOMD_ENV || '',
-    workerVersion: workerInfo.data.version || '',
-    workerGitHash: workerInfo.data.gitHash || ''
+    // Log environment variables for debugging
+    const envVars = [
+      'SFAPI_TOKEN_EXPIRES',
+      'USE_NERSC',
+      'NERSC_PROJECT',
+      'SENDMAIL_USER',
+      'GIT_HASH',
+      'BILBOMD_ENV',
+      'WORKER_SERVICE_URL',
+      'WORKER_SERVICE_PORT',
+    ];
+
+    envVars.forEach((envVar) => {
+      logger.info(`${envVar}: ${process.env[envVar]}`);
+    });
+
+    // Construct the response object
+    const configs = {
+      tokenExpires: process.env.SFAPI_TOKEN_EXPIRES || '2024-05-22 04:20',
+      useNersc: process.env.USE_NERSC || 'false',
+      nerscProject: process.env.NERSC_PROJECT || 'm1234',
+      sendMailUser: process.env.SENDMAIL_USER || 'bilbomd@lbl.gov',
+      gitHash: process.env.GIT_HASH || '',
+      mode: process.env.BILBOMD_ENV || '',
+      workerVersion: workerInfo.version || '',
+      workerGitHash: workerInfo.gitHash || '',
+    };
+
+    return res.json(configs);
+  } catch (error) {
+    logger.error('Error fetching worker info or processing request:', error);
+    return res.status(500).json({
+      message: 'Failed to retrieve configuration information',
+      error: error,
+    });
   }
-
-  // If the environment variable is not set, return an error
-  // if (!tokenExpires) {
-  //   return res
-  //     .status(500)
-  //     .json({ message: 'SFAPI token expiration date not set in environment variables' })
-  // }
-
-  return res.json(configs)
-}
+};
