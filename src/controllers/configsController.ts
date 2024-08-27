14a1f8e319b6254c65d028d6fa2c85454db1e75a
Scott Classen <sclassen@lbl.gov>
2024-08-26 17:08:48 -0700

    return worker version and githash

--- src/controllers/configsController.ts
+++ src/controllers/configsController.ts
@@ -1,18 +1,23 @@
 import { Request, Response } from 'express'
 import { logger } from '../middleware/loggers.js'
+import axios from 'axios'
 
-export const getConfigsStuff = (req: Request, res: Response) => {
-  logger.info(`process.env.SFAPI_TOKEN_EXPIRES: ${process.env.SFAPI_TOKEN_EXPIRES}`);
-  logger.info(`process.env.USE_NERSC: ${process.env.USE_NERSC}`);
-  logger.info(`process.env.NERSC_PROJECT: ${process.env.NERSC_PROJECT}`);
+export const getConfigsStuff = async (req: Request, res: Response) => {
+  logger.info(`process.env.SFAPI_TOKEN_EXPIRES: ${process.env.SFAPI_TOKEN_EXPIRES}`)
+  logger.info(`process.env.USE_NERSC: ${process.env.USE_NERSC}`)
+  logger.info(`process.env.NERSC_PROJECT: ${process.env.NERSC_PROJECT}`)
+
+  const workerInfo = await axios.get('http://worker-service/worker-info')
 
   const configs = {
     tokenExpires: process.env.SFAPI_TOKEN_EXPIRES || '2024-05-22 04:20',
     useNersc: process.env.USE_NERSC || 'false',
-    nerscProject: process.env.NERSC_PROJECT || 'm4659',
+    nerscProject: process.env.NERSC_PROJECT || 'm1234',
     sendMailUser: process.env.SENDMAIL_USER || 'bilbomd@lbl.gov',
     gitHash: process.env.GIT_HASH || '',
     mode: process.env.BILBOMD_ENV || '',
+    workerVersion: workerInfo.data.version || '',
+    workerGitHash: workerInfo.data.gitHash || ''
   }
 
   // If the environment variable is not set, return an error
