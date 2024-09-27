27a2ff99a47828e4afa9799057cf235583af045e
shreyasprabhakar <shrprabh@ttu.edu>
2024-09-26 21:25:26 -0500

    Cleanup: Remove comments for new routes to streamline code

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -10,23 +10,12 @@ import {
   deleteUserByUsername
 } from '../controllers/usersController.js'
 import verifyJWT from '../middleware/verifyJWT.js'
-
 const router = express.Router()
-
 router.use(verifyJWT)
-
 router.route('/').get(getAllUsers).patch(updateUser)
-
 router.route('/:id').get(getUser).delete(deleteUserById)
-
 router.delete('/delete-user-by-username/:username', deleteUserByUsername)
-// Route for sending email change request
 router.post('/change-email', sendChangeEmailOtp)
-
-//Route for verifying OTP
 router.post('/verify-otp', verifyOtp)
-
-// Route for resending otp
 router.post('/resend-otp', resendOtp)
-
 export default router
