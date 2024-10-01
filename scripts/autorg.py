94bfe610e43b49125507db0c058c7bb323189eea
Scott Classen <sclassen@lbl.gov>
2024-10-01 10:34:41 -0700

    add these clamp functions for rg_min and rg_max for safety and to align with code from bilbomd-worker

--- scripts/autorg.py
+++ scripts/autorg.py
@@ -53,6 +53,12 @@ def calculate_rg(file_path, output_file):
         rg_min = round(rg * 0.8)
         rg_max = round(rg * 1.5)
 
+        # Clamp rg_min to be no less than 10 and no more than 100
+        rg_min = max(10, min(100, rg_min))
+
+        # Clamp rg_max to be no less than 10 and no more than 100
+        rg_max = max(10, min(100, rg_max))
+
         # Create a dictionary with the results
         result_dict = {"rg": round(rg), "rg_min": rg_min, "rg_max": rg_max}
 
