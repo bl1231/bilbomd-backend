35baecbdd1e27d32099cee857865af01d91be461
Scott Classen <sclassen@lbl.gov>
2024-10-17 08:56:48 -0700

    remove eslint jest stuff

--- eslint.config.js
+++ eslint.config.js
@@ -1,35 +1,9 @@
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import jestPlugin from 'eslint-plugin-jest'
 
 export default [
   { files: ['**/*.{ts}'] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  { ignores: ['dist/*', 'build/*', 'test/*'] },
-  {
-    plugins: {
-      jest: jestPlugin
-    }
-  },
-  {
-    rules: {
-      'jest/no-disabled-tests': 'warn',
-      'jest/no-focused-tests': 'error',
-      'jest/no-identical-title': 'error',
-      'jest/prefer-to-have-length': 'warn',
-      'jest/valid-expect': 'error'
-      // 'import/extensions': [
-      //   'error',
-      //   'ignorePackages',
-      //   {
-      //     js: 'always',
-      //     jsx: 'always',
-      //     ts: 'always',
-      //     tsx: 'always',
-      //     mjs: 'always'
-      //   }
-      // ]
-    }
-  }
+  { ignores: ['dist/*', 'build/*', 'test/*'] }
 ]
