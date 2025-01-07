d7bb7809a95744a69c6bc22cf3f31a6a38b03032
Scott Classen <sclassen@lbl.gov>
2025-01-07 09:22:53 -0800

    add source ip address and log violations

--- src/middleware/loginLimiter.ts
+++ src/middleware/loginLimiter.ts
@@ -1,5 +1,6 @@
 import rateLimit, { Options } from 'express-rate-limit'
 import { Request, Response, NextFunction } from 'express'
+import { logger } from './loggers.js'
 
 const loginLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
@@ -9,7 +10,13 @@ const loginLimiter = rateLimit({
       'Too many login attempts from this IP, please try again after a 60 second pause'
   },
   handler: (req: Request, res: Response, next: NextFunction, options: Options) => {
-    // logEvents(`Too Many Requests: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
+    const clientIp = (req.ip ?? '').includes('::ffff:')
+      ? (req.ip ?? '').split('::ffff:')[1]
+      : req.ip ?? ''
+    logger.error(
+      `Too Many Requests: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}\t${clientIp}`,
+      'errLog.log'
+    )
     res.status(options.statusCode).send(options.message)
   },
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
