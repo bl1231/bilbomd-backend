2c389a4a5b9c57a416a7d1ccd19c77a7dc0ea262
Scott Classen <sclassen@lbl.gov>
2025-04-21 09:54:17 -0700

    do not commit API tokens to GitHub... bad scott.

--- scripts/submit_api_job.sh
+++ scripts/submit_api_job.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 API_URL="http://localhost:3501/api/v1/external/jobs"
-API_TOKEN="ce57039f1fa0afd38dd765fc945d6e7c16eaa2f60309c468cd2342ec8cc81b6d"
+
 
 PDB_FILE=./test/data/pro_dna_complex.pdb
 DAT_FILE="./test/data/pro_dna_saxs.dat"
 INP_FILE="./test/data/my_const.inp"
 
 curl -X POST "$API_URL" \
-  -H "Authorization: Bearer $API_TOKEN" \
+  -H "Authorization: Bearer $BILBOMD_API_TOKEN" \
   -F "bilbomd_mode=pdb" \
   -F "title=API Test Job" \
   -F "pdb_file=@${PDB_FILE}" \
