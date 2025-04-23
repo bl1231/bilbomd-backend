b5c8ea8fe178cfa5af2a86f0c63651f84bc1f59d
Scott Classen <sclassen@lbl.gov>
2025-04-23 15:55:46 -0700

    refactor to allow naming of fasts file

--- src/controllers/jobs/utils/createFastaFile.ts
+++ src/controllers/jobs/utils/createFastaFile.ts
@@ -1,10 +1,13 @@
 import path from 'path'
 import fs from 'fs-extra'
 import { logger } from '../../../middleware/loggers.js'
-
 import { IAlphaFoldEntity } from '@bl1231/bilbomd-mongodb-schema'
 
-const createFastaFile = async (entities: IAlphaFoldEntity[], jobDir: string) => {
+const createFastaFile = async (
+  entities: IAlphaFoldEntity[],
+  jobDir: string,
+  fastaFileName: string = 'af-entities.fasta'
+) => {
   // Determine the header
   let header = ''
   if (entities.length === 1) {
@@ -15,23 +18,18 @@ const createFastaFile = async (entities: IAlphaFoldEntity[], jobDir: string) => 
 
   // Generate the sequence lines
   const sequenceLines = entities
-    .flatMap((entity) => {
-      return Array.from({ length: entity.copies }, () => entity.sequence)
-    })
-    .map((sequence, idx, arr) => {
-      return idx === arr.length - 1 ? sequence : `${sequence}:`
-    })
+    .flatMap((entity) => Array.from({ length: entity.copies }, () => entity.sequence))
+    .map((sequence, idx, arr) => (idx === arr.length - 1 ? sequence : `${sequence}:`))
     .join('\n')
 
   // Combine the header and sequences
   const fastaContent = `${header}\n${sequenceLines}`
 
   // Define the path for the FASTA file
-  const fastaFilePath = path.join(jobDir, 'af-entities.fasta')
+  const fastaFilePath = path.join(jobDir, fastaFileName)
 
   // Write the FASTA content to the file
   await fs.writeFile(fastaFilePath, fastaContent)
-
   logger.info(`FASTA file created: ${fastaFilePath}`)
 }
 
