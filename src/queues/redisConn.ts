e3fda9d5473202ebad436453f3eb43f3f81bae06
Scott Classen <sclassen@lbl.gov>
2025-06-05 13:00:22 -0700

    explicit set maxRetriesPerRequest: null

--- src/queues/redisConn.ts
+++ src/queues/redisConn.ts
@@ -6,7 +6,7 @@ const redisOptions: RedisOptions = {
       ? parseInt(process.env.REDIS_PORT, 10)
       : 6379,
   host: process.env.REDIS_HOST || 'localhost',
-  // password: process.env.REDIS_PASSWORD || '',
+  maxRetriesPerRequest: null,
   tls: process.env.REDIS_TLS ? JSON.parse(process.env.REDIS_TLS) : false
 }
 
